afe2e3f35c33ea9a0b4785a3d3de139cbb969dec
Dionkeldei <dionkeldei@gmail.com>
2021-03-09 15:05:36 -0600

    industry title

--- src/components/industry.vue
+++ src/components/industry.vue
@@ -1,5 +1,13 @@
 <template lang="html">
-  <div class="uk-width-1-1 background-industry" :style="backgroundImage()">
+  <div class="uk-width-1-1 uk-flex uk-flex-middle industry-div" style="height:100%" uk-grid>
+    <div class="uk-width-2-3" style="height:100%">
+      <img :src="industry.image" style="max-height:100%"/>
+    </div>
+    <div class="uk-width-1-3">
+      <p class="uk-text-center title-industry">{{industry.name}}</p>
+      <p class="uk-text-center">Ver productos</p>
+      <p class="uk-text-center"><span uk-icon="icon: chevron-down"></span></p>
+    </div>
 
   </div>
 </template>
@@ -11,18 +19,21 @@ export default {
     industry: Object
   },
   methods: {
-    backgroundImage: function () {
-      return 'background-image: url("'+this.industry.image+'");'
-    }
   }
 }
 </script>
 
 <style lang="css" scoped>
-.background-industry{
-  height: 500px; /* You must set a specified height */
-  background-position: left; /* Center the image */
-  background-repeat: no-repeat; /* Do not repeat the image */
-  background-size: cover; /* Resize the background image to cover the entire container */
+h3,p{
+  line-height:11px !important;
+}
+.industry-div{
+  cursor: pointer;
+}
+.industry-div:hover{
+  color: red !important;
+}
+.title-industry{
+  font-size: 1.5rem;
 }
 </style>
